54a6ec9225693fae64d89ac6de84f826cb84137d
Spyros Ioakeimidis <spyrosikmd@gmail.com>
2018-08-11 17:12:50 +0200

    Write tests to cover more cases
    Test specifically the Blob response to make sure the use of
    `cross-fetch` works.`

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
 const fetch = require('cross-fetch');
 
 function request() {
-  return fetch('https://randomuser.me/api', {}).then(response =>
-    response.json()
-  );
+  return fetch('https://randomuser.me/api', {})
+    .then(response => {
+      const contentType = response.headers.get('content-type');
+
+      if (/application\/json/.test(contentType)) {
+        return response.json();
+      }
+
+      if (/text\/csv/.test(contentType)) {
+        return response.blob();
+      }
+
+      if (!contentType || /^text\/|charset=utf-8$/.test(contentType)) {
+        return response.text();
+      }
+
+      return response;
+    })
+    .catch(error => {
+      const errorData = JSON.parse(error);
+      throw new Error(errorData.error);
+    });
 }
 
 module.exports = request;
--- src/index.test.js
+++ src/index.test.js
@@ -25,4 +25,49 @@ describe('request', () => {
       })
       .catch(done.fail);
   });
+
+  it('returns text when response is text', done => {
+    fetch.mockResponseOnce('ok');
+
+    request()
+      .then(response => {
+        expect(fetch).toHaveBeenCalledWith('https://randomuser.me/api', {});
+        expect(response).toEqual('ok');
+        done();
+      })
+      .catch(done.fail);
+  });
+
+  it('returns blob when response is text/csv', done => {
+    const contentType = 'text/csv; charset=utf-8';
+    fetch.mockResponseOnce('csv data', {
+      headers: {
+        'Content-Type': contentType
+      }
+    });
+
+    request()
+      .then(response => {
+        expect(fetch).toHaveBeenCalledWith('https://randomuser.me/api', {});
+        // Blob responses have a type.
+        expect(response.type).toBe(contentType);
+        done();
+      })
+      .catch(done.fail);
+  });
+
+  it('rejects with error data', done => {
+    const errorData = {
+      error:
+        'Uh oh, something has gone wrong. Please tweet us @randomapi about the issue. Thank you.'
+    };
+    fetch.mockRejectOnce(JSON.stringify(errorData));
+
+    request()
+      .then(done.fail)
+      .catch(error => {
+        expect(error.message).toBe(errorData.error);
+        done();
+      });
+  });
 });
